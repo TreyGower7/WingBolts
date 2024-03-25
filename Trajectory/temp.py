53300f6a548d2e84e10f08063533ea6e7596713f
Niccole Riera <nriera@utexas.edu>
2024-03-25 15:02:17 -0500

    starting rough code for trajectory

--- Trajectory/temp.py
+++ Trajectory/temp.py
@@ -1,1 +1,19 @@
-# test
+import numpy as np
+
+def calc_trajectory():
+    """
+    Using dummy variables until we find out how to get the data from Pixhawk
+    """
+    # all of the following are recorded at a given instant and should come from Pixhawk
+
+    u = 1; # aircraft velocity (relative to ground)
+    H = 1; # aircraft altitude (above ground level)
+    w = 1; #wind speed (relative to ground)
+
+    # these are constant parameters that should be stored beforehand
+
+if __name__ == "__main__":
+    calc_trajectory()
+
+
+
