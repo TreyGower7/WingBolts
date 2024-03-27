e0fd6c65280dc09405654db629d946c43dba6771
Trey gower <goweryert@gmail.com>
2024-03-27 18:35:37 -0500

    Updated return of coords

--- Flight_pathing/pathing/Random_Lat_lon.py
+++ Flight_pathing/pathing/Random_Lat_lon.py
@@ -11,13 +11,13 @@ def random_coords(domain):
     Tuple (latitude, longitude)
     """
     min_lat, max_lat, min_lon, max_lon = domain
-    latitude = random.uniform(min_lat, max_lat)
-    longitude = random.uniform(min_lon, max_lon)
-    return latitude, longitude
+    lat = random.uniform(min_lat, max_lat)
+    lon = random.uniform(min_lon, max_lon)
+    coords = {'latitude': lat, 'longitude': lon}
+
+    return coords
 
 # Example usage:
 domain = (30.320122, 30.324865, -97.603076, -97.598687)  # Boundaries for Arca
-lat, lon = random_coords(domain)
-coords = [lat, lon]
-
+coords = random_coords(domain)
 print(coords)
