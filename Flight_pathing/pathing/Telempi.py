1fe4f44b5e7bb95f01ca224b83069c186b327e92
Trey gower <goweryert@gmail.com>
2024-04-05 11:04:06 -0500

    updates to telem.py for simulation

--- Flight_pathing/pathing/Telempi.py
+++ Flight_pathing/pathing/Telempi.py
@@ -1,24 +1,13 @@
 from RandLatlon import random_coords
 from pymavlink import mavutil
 import time
-"""
-Updating pixhawk from our ML model
-"""
 
+# Set the connection parameters (change accordingly)
+connection_string = '/dev/ttyUSB0'  # or '/dev/ttyAMA0' for UART connection
+baudrate = 115200  # or whatever baudrate your connection uses
 
-#Connect Pi to Pixhawk
-
-
-connect = mavutil.mavlink_connection('udpin:10.0.1.128:14550')
-
-# Waits for first heartbeat **only need loop for viewing purposes**
-while not connect.wait_heartbeat():
-    print('Waiting for Connection')
-    time.sleep(1)
-
-print(f'Heartbeat from system: {connect.target_system()}, component: {connect.target_component()} ')
-
-__author__ = "Trey Gower"
+# Connect to the Pixhawk
+master = mavutil.mavlink_connection(connection_string, baud=baudrate)
 
 def get_telem():
     '''
@@ -32,34 +21,58 @@ def send_telem(coords):
     '''
     sends telemetry data to pixhawk from pi
     '''
-     # Send telemetry data to Pixhawk
-    msg = mavutil.mavlink.MAVLink_mission_item_message(
+    altitude = altitude_handle()
+    # Send telemetry data to Pixhawk
+    msg = master.mav.mission_item_send(
         0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
         mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0,
-        coords['latitude'], coords['longitude'], coords['altitude'])
-    connect.mav.send(msg)
+        coords['latitude'], coords['longitude'], altitude)
+    master.mav.send(msg)
 
-def recieve_telem():
+def receive_telem():
     '''
-    recieves telemetry data to pixhawk from pi
+    receives telemetry data from pixhawk to pi
     '''
-    #Receive a message
-    msg = connect.recv_match()
+    # Receive messages in a loop
+    while True:
+        # Wait for a message
+        msg = master.recv_match()
         
-        #Check if message is not None
-    if msg:
-        #Print the message
-        print(msg)
-    return msg
+        # Check if message is not None
+        if msg:
+            # Process messages of interest
+            if msg.get_type() == 'GLOBAL_POSITION_INT':
+                # Example: Print latitude, longitude, and altitude
+                print("Global Position: Lat={}, Lon={}, Alt={}".format(msg.lat, msg.lon, msg.alt))
+            elif msg.get_type() == 'STATUSTEXT':
+                # Example: Print status text messages
+                print("Status Text: {}".format(msg.text))
+            # Add more conditions to handle other message types as needed
+            # elif msg.get_type() == 'SOME_OTHER_MESSAGE_TYPE':
+            #    ...
+            
+            # Example: Check if message is a heartbeat
+            if msg.get_type() == 'HEARTBEAT':
+                print("Received heartbeat from system {} component {}".format(msg.get_srcSystem(), msg.get_srcComponent()))
+                
+        # Add other logic here if needed
+        
+        # Sleep for a short duration to avoid busy-waiting
+        time.sleep(0.1)
     
 def altitude_handle():
     '''
     handles the altitude inputs to the plane
     '''
+    return 45.72  #Altitudes are in meters
 
 def main():
     ''' Main Func '''
-    coords = get_telem()
+    while True:
+        coords = get_telem()
+        send_telem(coords)
+        time.sleep(5)  #Adjust as needed for the update frequency
+
 if __name__ == "__main__":
     ''' This is executed when run from the command line '''
     main()
