c8bbea0f8faca8847b1e1de613e0e500bc0d55e2
Niccole Riera <nriera@utexas.edu>
2024-04-12 12:28:05 -0500

    changes to python

--- Trajectory/trajectory.py
+++ Trajectory/trajectory.py
@@ -4,14 +4,6 @@ import numpy as np
 import math
 import random
 
-connection_string = '/dev/ttyUSB0'  # or '/dev/ttyAMA0' for UART connection
-baudrate = 115200  # or whatever baudrate your connection uses
-
-# Connect to the Pixhawk
-master = mavutil.mavlink_connection(connection_string, baud=baudrate)
-
-
-
 def random_coords(domain):
     """
     *********************
@@ -39,59 +31,6 @@ def get_telem():
     coords = random_coords(domain)
     return coords
 
-def send_telem(coords):
-    '''
-    sends telemetry data to pixhawk from pi
-    '''
-    altitude = altitude_handle('surveillance')
-    # Send telemetry data to Pixhawk
-    msg = master.mav.mission_item_send(
-        0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
-        mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0,
-        coords['latitude'], coords['longitude'], altitude)
-    master.mav.send(msg)
-
-def receive_telem():
-    '''
-    receives telemetry data from pixhawk to pi
-    '''
-    # Receive messages in a loop
-    while True:
-        # Wait for a message
-        msg = master.recv_match()
-        
-        # Check if message is not None
-        if msg:
-            # Process messages of interest
-            if msg.get_type() == 'GLOBAL_POSITION_INT':
-                # Example: Print latitude, longitude, and altitude
-                print("Global Position: Lat={}, Lon={}, Alt={}".format(msg.lat, msg.lon, msg.alt))
-            elif msg.get_type() == 'STATUSTEXT':
-                # Example: Print status text messages
-                print("Status Text: {}".format(msg.text))
-            # Add more conditions to handle other message types as needed
-            # elif msg.get_type() == 'SOME_OTHER_MESSAGE_TYPE':
-            #    ...
-            
-            # Example: Check if message is a heartbeat
-            if msg.get_type() == 'HEARTBEAT':
-                print("Received heartbeat from system {} component {}".format(msg.get_srcSystem(), msg.get_srcComponent()))
-                
-        # Add other logic here if needed
-        
-        # Sleep for a short duration to avoid busy-waiting
-        time.sleep(0.1)
-    
-def altitude_handle(phase):
-    '''
-    handles the altitude inputs to the plane
-    '''
-    #Altitudes are in meters
-    if phase == 'search':
-        return 76.2
-    if phase == 'surveillance':
-        return 45.72  
-
 def projectile_range(v_x, v_y, H):
     """
     Using random numbers for the variables as placeholders until we find out how to get the data from Pixhawk
@@ -99,11 +38,11 @@ def projectile_range(v_x, v_y, H):
     """
 
     # these are constant parameters that should be stored beforehand
-    rho = 1.225 # kg/m density of air
-    Cd = 0 # drag coefficient, proportional to airspeed?
+    rho = 1.225 # kg/m^3 density of air
+    Cd = 0.82 # drag coefficient, proportional to airspeed?
 
     # A does not include the grid fins yet
-    A = 0.0157 # surface area of the payload in m^2 (2 in diameter, 3 in length)
+    A = 0.016214 # surface area of the payload in m^2 (2 in diameter, 3 in length)
     m = 0.181 # payload mass in kg (0.4 lb)
 
     q = 0.5*rho*Cd*A
@@ -119,7 +58,7 @@ def projectile_range(v_x, v_y, H):
         if (y >= H):
             R = x
             break
-
+        
         a_x = -(q/m)*v_x**2
         a_y = 9.81 - (q/m)*v_y**2
 
@@ -129,12 +68,14 @@ def projectile_range(v_x, v_y, H):
         x += v_x*dt + 0.5*a_x*dt**2
         y += v_y*dt + 0.5*a_y*dt**2
 
+        print(iters, a_y, x, y, v_x, v_y)
+
         t += dt # total time, don't know what this is useful for?
     
 
         iters += 1
 
-    return(R,t)
+    return(R)
 
 def release_point(target_long, target_lat, R, current_long, current_lat):
     z = (target_long - current_long)/(target_lat - current_lat)
