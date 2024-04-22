225178864706183205e8295da1c0c382c3fa6177
Trey gower <goweryert@gmail.com>
2024-04-22 11:19:47 -0500

    Niccole Insert trajectory script where indicated

--- Flight_pathing/pathing/Phases.py
+++ Flight_pathing/pathing/Phases.py
@@ -2,6 +2,8 @@ from pymavlink import mavutil
 import random
 import time
 from Flight_pathing.pathing.Telempy import send_telem
+from Flight_pathing.pathing.Sorting_Distance import sort_obj_waypoints
+
 
 
 def Search_zigzag(master):
@@ -21,6 +23,41 @@ def Search_zigzag(master):
         send_telem(coords, phase)
 
     return waypoints
+
+def predrop_phase(refinedobj_waypoints, phase):
+    '''
+    Sets the plane up for dropping the payload
+    '''
+    distressed_waypoints = []
+    happy_waypoints = []
+
+    #fly out to random point to prepare for payload drop
+    reset_waypoint = {'lat': 30.323246847038178, 'lon': -97.60228430856947}
+    send_telem(reset_waypoint, phase)
+    print(f"Sending waypoint: ({reset_waypoint[i]['lat']}, {reset_waypoint[i]['lon']})")
+
+    #Sort and send distressed targets
+    #****Write these to a file to be saved***** 
+    for i in range(len(refinedobj_waypoints)):
+        if refinedobj_waypoints[i]['state'] == 'distressed':
+            distressed_waypoints.append(refinedobj_waypoints[i])
+        else:
+            happy_waypoints.append(refinedobj_waypoints[i])
+
+     #***Calculate drop points here in a loop and store them based on distressed_waypoints***
+    #first drop point based on reset point, second drop point based on first object waypoint...
+    drop_points = {'lat': 0, 'lon': 0}
+
+    #Sort by distance
+    drop_points = sort_obj_waypoints(reset_waypoint, drop_points)
+    #Send sorted drop_point for target coords
+    for i in range(len(drop_points)):
+        #****Need to Add Loitering****
+        coords = {'latitude': drop_points[i]['lat'], 'longitude': drop_points[i]['lon']}
+        send_telem(coords, phase)
+        print(f"Sending waypoint {i+1}: ({drop_points[i]['lat']}, {drop_points[i]['lon']})")
+    
+    return drop_points
 
 
 
