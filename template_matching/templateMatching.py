68f7394f78830cfa4ac6663586353ed2f80677a3
stefcatalan <stefanie.catalan@utexas.edu>
2024-04-23 14:44:58 -0500

    able to differentiate between happy and sad now

--- template_matching/templateMatching.py
+++ template_matching/templateMatching.py
@@ -17,47 +17,10 @@ w = templateShapes[0][1]
 h = templateShapes[0][0]
 
 # methods of computation: which one do i use ;-;
-# method = cv.TM_CCOEFF # bad for video, okay for image
-method = cv.TM_CCOEFF_NORMED # decent for video, bad for image
-# method = cv.TM_CCORR # doesnt always have box, but cant find target
-# method = cv.TM_CCORR_NORMED # about as accurate as cv.TM_CCOEFF_NORMED
-# method = cv.TM_SQDIFF # needs masking
-# method = cv.TM_SQDIFF_NORMED # needs masking
-
-# testing image
-img = cv.imread("testhappy.png", cv.IMREAD_GRAYSCALE)
+method = cv.TM_CCOEFF_NORMED # decent for video, meh for image
 
 # testing video
 vid = cv.VideoCapture("testclip.mp4")  
-
-"""
-this function ...
-
-    - takes in an image as input
-    - performs template matching on the image for each template
-"""
-
-def templateMatching(templates, img, w, h):
-
-    for i in templates:
-
-        # do template matching
-        res = cv.matchTemplate(img, i, method)
-
-        # store coordinates of matched area
-        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-
-        # calculate coordinates
-        # top_left = min_loc # for SQDIFF methods
-        top_left = max_loc
-        bottom_right = (top_left[0] + w, top_left[1] + h)
-
-        # add bounding box
-        cv.rectangle(img, top_left, bottom_right, (255, 0, 0), 2)
-
-        # view image
-        plt.imshow(img)
-        plt.show()
 
 """
 this function ...
@@ -66,7 +29,10 @@ this function ...
     - performs template matching on each frame of the video
 """
 
-def videoTemplateMatching(templates, vid, w, h):
+def videoTemplateMatching(templates, vid, method, w, h):
+
+    # how speecific template match is
+    threshold = 0.8
     
     while(True):  
 
@@ -75,19 +41,26 @@ def videoTemplateMatching(templates, vid, w, h):
     
         # make each frame grayscale
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)  
-
-        # do template matching
-        res = cv.matchTemplate(gray, templates[0], method)
-
-        # treshold for 
-        threshold = 0.8
         
-        # store coordinates of matched area
-        loc = np.where(res >= threshold) 
+        # template matching for happy and sad facecs
+        res1 = cv.matchTemplate(gray, templates[0], method)
+        res2 = cv.matchTemplate(gray, templates[1], method)
 
-        # add bounding box in matched area
-        for pt in zip(*loc[::-1]): 
-            cv.rectangle(gray, pt, (pt[0] + w, pt[1] + h), (255, 0, 0), 2) 
+        # store coordinates of matched area
+        loc1 = np.where(res1 >= threshold) 
+        loc2 = np.where(res2 >= threshold) 
+
+        # only when a happy target is detected
+        for pt in zip(*loc1[::-1]): 
+                
+                # add bounding box in matched area
+                cv.rectangle(gray, pt, (pt[0] + w, pt[1] + h), (255, 0, 0), 2)        
+
+        # only when a sad target is detected
+        for pt in zip(*loc2[::-1]): 
+                
+                # add bounding box in matched area
+                cv.rectangle(gray, pt, (pt[0] + w, pt[1] + h), (0, 255, 255), 2)
 
         # display frames
         cv.imshow('frame', gray)  
@@ -99,6 +72,7 @@ def videoTemplateMatching(templates, vid, w, h):
     cv.destroyAllWindows()  
 
 if __name__ == "__main__":
-    # templateMatching(templates, img, w, h)
-    videoTemplateMatching(templates, vid, w, h)
+
+    # call function
+    videoTemplateMatching(templates, vid, method, w, h)
 
