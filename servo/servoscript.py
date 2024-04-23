d8c8f3301e473820575ba43ff203b2874ac742a5
Steven Oh <91847206+jaeestee@users.noreply.github.com>
2024-04-23 16:40:21 -0500

    Update servoscript.py

--- servo/servoscript.py
+++ servo/servoscript.py
@@ -4,12 +4,12 @@ from time import sleep
 servo = Servo(26)
 
 try:
-	while True:
-    	servo.min()
-    	sleep(0.5)
-    	servo.mid()
-    	sleep(0.5)
-    	servo.max()
-    	sleep(0.5)
+   while True:
+   servo.min()
+   sleep(0.5)
+   servo.mid()
+   sleep(0.5)
+   servo.max()
+   sleep(0.5)
 except KeyboardInterrupt:
-	print("Program stopped")
+   print("Program stopped")
