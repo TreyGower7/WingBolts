933b6c2b1236be353512f4b9f0e2be1d2e35b407
stefcatalan <stefanie.catalan@utexas.edu>
2024-04-24 11:28:03 -0500

    now writes coordinates to file

--- template_matching/templateMatching.py
+++ template_matching/templateMatching.py
@@ -1,6 +1,8 @@
-from matplotlib import pyplot as plt
 import numpy as np
 import cv2 as cv
+import json
+from ..Flight_pathing.pathing import Telempy as tp
+
 
 # set happy and sad templates
 templates = []
@@ -11,10 +13,6 @@ templates.append(cv.imread("sad.png", cv.IMREAD_GRAYSCALE))
 templateShapes = []
 templateShapes.append(templates[0].shape[:: -1])
 templateShapes.append(templates[1].shape[:: -1])
-
-# dimensions for bounding box calculations
-w = templateShapes[0][1]
-h = templateShapes[0][0]
 
 # methods of computation: which one do i use ;-;
 method = cv.TM_CCOEFF_NORMED # decent for video, meh for image
@@ -27,9 +25,10 @@ this function ...
 
     - takes in a video as input
     - performs template matching on each frame of the video
+    - writes coordinates of bounding boxes to file
 """
 
-def videoTemplateMatching(templates, vid, method, w, h):
+def videoTemplateMatching(templates, vid, method):
 
     # how speecific template match is
     threshold = 0.8
@@ -41,7 +40,7 @@ def videoTemplateMatching(templates, vid, method, w, h):
     
         # make each frame grayscale
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)  
-        
+
         # template matching for happy and sad facecs
         res1 = cv.matchTemplate(gray, templates[0], method)
         res2 = cv.matchTemplate(gray, templates[1], method)
@@ -52,15 +51,74 @@ def videoTemplateMatching(templates, vid, method, w, h):
 
         # only when a happy target is detected
         for pt in zip(*loc1[::-1]): 
+
+            # plane coordinates
+            plane = tp.receive_telem()
+            curr_lat = plane.lat
+            curr_lon = plane.lon
+            curr_alt = plane.alt
+
+            # dimensions for bounding box calculations
+            w = templateShapes[0][1]
+            h = templateShapes[0][0]
                 
-                # add bounding box in matched area
-                cv.rectangle(gray, pt, (pt[0] + w, pt[1] + h), (255, 0, 0), 2)        
+            # add bounding box in matched area
+            cv.rectangle(gray, pt, (pt[0] + w, pt[1] + h), (255, 0, 0), 2)
+
+            # top left corner point
+            top_left = pt
+
+            # bottom right corner point
+            bottom_right = (pt[0] + w, pt[1] + h)
+
+            # data for json file
+            dictionary = {
+                "curr_lat": curr_lat,
+                "curr_lon": curr_lon,
+                "curr_alt": curr_alt,
+                "top_left": top_left,
+                "top_right": (bottom_right[0], top_left[1]),
+                "bottom_left": (top_left[0], bottom_right[1]),
+                "bottom_right": bottom_right
+            }
+            
+            # serializing json
+            json_object = json.dumps(dictionary, indent=4, default=str)
+
+            # write to json file
+            with open("happycoordinates.json", "w") as outfile:
+                outfile.write(json_object)
 
         # only when a sad target is detected
         for pt in zip(*loc2[::-1]): 
+
+            # dimensions for bounding box calculations
+            w = templateShapes[1][1]
+            h = templateShapes[1][0]
                 
-                # add bounding box in matched area
-                cv.rectangle(gray, pt, (pt[0] + w, pt[1] + h), (0, 255, 255), 2)
+            # add bounding box in matched area
+            cv.rectangle(gray, pt, (pt[0] + w, pt[1] + h), (0, 255, 255), 2)
+
+            # top left corner point
+            top_left = pt
+
+            # bottom right corner point
+            bottom_right = (pt[0] + w, pt[1] + h)
+
+            # data for json file
+            dictionary = {
+                "top_left": top_left,
+                "top_right": (bottom_right[0], top_left[1]),
+                "bottom_left": (top_left[0], bottom_right[1]),
+                "bottom_right": bottom_right
+            }
+            
+            # serializing json
+            json_object = json.dumps(dictionary, indent=4, default=str)
+
+            # write to json file
+            with open("sadcoordinates.json", "w") as outfile:
+                outfile.write(json_object)
 
         # display frames
         cv.imshow('frame', gray)  
@@ -74,5 +132,5 @@ def videoTemplateMatching(templates, vid, method, w, h):
 if __name__ == "__main__":
 
     # call function
-    videoTemplateMatching(templates, vid, method, w, h)
+    videoTemplateMatching(templates, vid, method)
 
