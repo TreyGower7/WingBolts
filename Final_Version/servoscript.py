007bc98c397cad9099b577ccb2b3d93c63fa564b
Trey gower <goweryert@gmail.com>
2024-04-26 10:55:12 -0500

    try

--- Final_Version/servoscript.py
+++ Final_Version/servoscript.py
@@ -1,18 +1,29 @@
 from gpiozero import Servo
 from time import sleep
 
-def servo_activate():
-   servo = Servo(26)
-   try:
-      #while True:
-      servo.min(1193)
-      print('Servo Min')
-      sleep(0.5)
-      servo.mid(1556)
-      print('Servo Mid')
-      sleep(0.5)
-      print('Servo Max')
-      servo.max()
-      sleep(0.5)
-   except KeyboardInterrupt:
-      print("Program stopped")
+from gpiozero import Servo
+from time import sleep
+
+# Define servo and PWM values
+servo = Servo(26)
+neutral_pwm = 0   # Neutral position
+adp1_pwm = -1     # ADP1 position
+adp2_pwm = 1      # ADP2 position
+
+try:
+    while True:
+        servo.value = neutral_pwm
+        print("Moved to Neutral position")
+        sleep(2)
+
+        servo.value = adp1_pwm
+        print("Moved to ADP1 position")
+        sleep(2)
+
+        servo.value = adp2_pwm
+        print("Moved to ADP2 position")
+        sleep(2)
+
+except KeyboardInterrupt:
+    servo.detach()
+
