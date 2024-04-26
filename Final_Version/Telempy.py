70001da84914ab9ea352be4213b89f5f43b71f13
Trey gower <goweryert@gmail.com>
2024-04-26 08:26:02 -0500

    maybe working

--- Final_Version/Telempy.py
+++ Final_Version/Telempy.py
@@ -21,29 +21,30 @@ def check_AUTO(master):
     
 #****Tested and works*****
 def send_telem(master, waypoints,phase):
+    wp = mavwp.MAVWPLoader()   
     altitude = altitude_handle(phase)                                                 
-       # Clear existing waypoints
-    master.mav.mission_clear_all_send(
-        master.target_system, master.target_component)
-    time.sleep(1)
+    seq = 1
+    frame = mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT
+    radius = 10
+    N = len(waypoints)
+    for i in range(N):                  
+        wp.add(mavutil.mavlink.MAVLink_mission_item_message(master.target_system,
+                    master.target_component,
+                    seq,
+                    frame,
+                    mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
+                    0, 0, 0, radius, 0, 0,
+                    waypoints[i]['lat'],waypoints[i]['lon'], altitude))
+        seq += 1                                                                       
 
-    # Send new waypoints
-    seq = 0
-    for i in range(len(waypoints)):
-        master.mav.mission_item_send(
-            master.target_system, master.target_component,
-            seq,
-            mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
-            mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
-            0, 0, 0, 0, 0, 0,
-            waypoints[i]['lat'], waypoints[i]['lon'], altitude)
-        seq += 1
-        time.sleep(0.2)  # Add a small delay to ensure waypoints are sent sequentially 
-    while True:
-        msg = master.recv_match(type=['MISSION_ACK'], blocking=True)
-        if msg:
-            print("Waypoints updated successfully")
-            break
+    master.waypoint_clear_all_send()                                     
+    master.waypoint_count_send(wp.count())                          
+
+    for i in range(wp.count()):
+        msg = master.recv_match(type=['MISSION_REQUEST'],blocking=True)             
+        master.mav.send(wp.wp(msg.seq))                                                                      
+        print(f'Sending waypoint {msg.seq}')  
+        
 #****Tested and works*****
 def receive_telem(master):
     '''
